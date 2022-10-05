7429ef62bc528edcb92e192d0051c4cf451dd5bc
최지훈 <cjh0630@naver.com>
2022-10-05 17:57:40 +0900

    Ch15_Service

---
+++ ch15_outer/src/main/java/com/example/ch15_outter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ch15_outter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ch15_outter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
